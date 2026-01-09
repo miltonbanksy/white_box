bd8c2960b9ec06a4f0f26b51c4ca1afeb12bb633
miltonbanksy <miltonbanksy@gmail.com>
2026-01-09 17:09:40 +0900

    Add initial monster data for Kobold and template structure

---
+++ whitebox_monsters.js
@@ -0,0 +1,39 @@
+// Template
+/*
+{
+        name: "",
+        form: "",
+        hit_dice: "",
+        HDE: "",
+        toHitBonus: "",
+        armor_class_descend: ,
+        armor_class_ascend: ,
+        attack: "weapon",
+        special: "none",
+        move: "",
+        XP: ,
+        saving_throw: "",
+        terrain: ["settled", "Hills, Mtns, Barren", "Swamp, Jungle", "Open Forest"],
+        notes: ""
+    }
+*/
+
+
+const arrayMonsters = [
+    {
+        name: "Kobold",
+        form: "humanoid",
+        hit_dice: "Half",
+        HDE: "<1",
+        toHitBonus: "+0",
+        armor_class_descend: 6,
+        armor_class_ascend: 13,
+        attack: "weapon",
+        special: "none",
+        move: "6",
+        XP: 10,
+        saving_throw: "19 - HD",
+        terrain: ["settled", "Hills, Mtns, Barren", "Swamp, Jungle", "Open Forest"],
+        notes: "Kobolds are subterranean, vaguely goblin-like humanoids. They have a -1 “to-hit” penalty when fighting above ground. Many use slings or short bows, and they fight with short swords or spiked clubs in melee combat."
+    }
+];
